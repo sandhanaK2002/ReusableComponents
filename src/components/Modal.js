88536f140d762aeacbb631fd2e522c3173ef6644
sandhanaK2002 <sandhanakrishnan010@gmail.com>
2023-05-12 20:31:16 +0530

    Reusable Component

---
+++ src/components/Modal.js
@@ -0,0 +1,16 @@
+function Modal() {
+
+
+  return (
+    <>  
+      <div className = "absolute inset-0  bg-blue-300 opacity-25">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse quisquam nobis in ex ullam exercitationem cum. Amet fugiat dolore dolorem dicta, quis alias nihil vel odio. Reiciendis, repellat aut cum quas assumenda itaque optio sed quidem adipisci, quod perspiciatis nihil molestias ad iure neque necessitatibus nesciunt eum tempore libero corrupti?
+      </div> 
+      <div className= "absolute inset-40 bg-white-500">
+          <button>Accept</button>
+        </div>
+    </>  
+    )  
+}
+
+export default Modal;
